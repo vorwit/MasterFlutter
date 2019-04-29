9428ff4ed2c7e0813b4545ee8bc8945ab478f645
vorwitJD <vorwit.j16@gmail.com>
2019-04-29 13:27:34 +0700

    after lunch

---
+++ android/app/src/main/java/th/go/dopa/vorwitfirebase/MainActivity.java
@@ -0,0 +1,13 @@
+package th.go.dopa.vorwitfirebase;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
